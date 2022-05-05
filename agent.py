a2ed72a1f78ce8a506718d8890374bb295d1c9de
Tom Liessmann <tomliessmann@gmail.com>
2022-05-05 18:00:46 +0200

    Implemented diagonal gaussian stochasticpolicy with pytorch

--- agent.py
+++ agent.py
@@ -1,16 +1,77 @@
-from typing import Tuple
 import numpy as np
+import torch
+from torch import nn
+from torch.distributions import Normal
 
 from rlbench.backend.observation import Observation
 
 
-class Agent:
-  def __init__(self, action_shape: Tuple[int]) -> None:
-      self.action_shape = action_shape
+class DiagonalGaussian(nn.Module):
+  def __init__(
+    self,
+    obs_dim: int,
+    hidden_dim: int,
+    action_dim: int,
+    activation
+  ) -> None:
+      super(DiagonalGaussian, self).__init__()
+      log_std = -0.5 * np.ones(action_dim, dtype=np.float32)
+      self.covariance_matrix = torch.nn.Parameter(torch.as_tensor(log_std))
+      self.mean_action_net = nn.Sequential(
+        nn.Linear(obs_dim, hidden_dim),
+        activation(),
+        nn.Linear(hidden_dim, action_dim),
+        activation())
 
-  def act(self, obs: Observation) -> np.ndarray:
-    """TODO: Implement simple network using a singular input from Observation
-    """
-    arm = np.random.normal(0.0, 0.1, size=(self.action_shape[0] - 1,))
-    gripper = [1.0]  # Always open
-    return np.concatenate([arm, gripper], axis=-1)
+  def _distribution(self, observation):
+      mean_act = self.mean_action_net(observation)
+      covariance_mat = torch.exp(self.covariance_matrix)
+      return Normal(mean_act, covariance_mat)
+
+  def _log_probs_from_dist(self, policy_dist, action):
+      return policy_dist.log_prob(action).sum(axis=-1)
+
+  def forward(self, observation, action=None):
+      policy_dist = self._distribution(observation)
+      logp_act = None
+      if action is not None:
+          logp_act = self._log_probs_from_dist(policy_dist, action)
+      return policy_dist, logp_act
+
+
+class ValueFunctionLearner(nn.Module):
+  def __init__(self, obs_dim: int, hidden_dim: int, action_dim: int, activation) -> None:
+      super(ValueFunctionLearner, self).__init__()
+      self.v_net = nn.Sequential(
+        nn.Linear(obs_dim, hidden_dim),
+        activation(),
+        nn.Linear(hidden_dim, action_dim),
+        activation()
+      )
+
+  def forward(self, observation):
+      # return torch.squeeze(self.v_net(observation), -1)
+      return self.v_net(observation)
+
+
+class Agent(nn.Module):
+  def __init__(
+    self,
+    obs_dim: int,
+    action_dim: int,
+    hidden_dim: int=32,
+    activation=nn.Softmax
+  ) -> None:
+    self.policy = DiagonalGaussian(obs_dim, hidden_dim, action_dim, activation)
+    self.value_func = ValueFunctionLearner(obs_dim, hidden_dim, action_dim, activation)
+  
+  def step(self, obs: Observation):
+    with torch.no_grad():
+      policy_dist = self.policy._distribution(obs)
+      action = policy_dist.sample()
+      mean_action = self.policy._log_probs_from_dist(policy_dist, action)
+      value = self.value_func(obs)
+    return action.numpy(), value.numpy(), mean_action.numpy()
+
+  def act(self, obs: Observation):
+    return self.step(obs)[0]
---
+++ reach_target_simple.py
@@ -0,0 +1,29 @@
+from typing import List, Tuple
+
+from rlbench.backend.task import Task
+from rlbench.backend.conditions import DetectedCondition
+
+from pyrep.objects import ProximitySensor
+
+
+class ReachTargetSimple(Task):
+
+    def init_task(self) -> None:
+        success_sensor = ProximitySensor('success')
+        success_condition = DetectedCondition(
+            obj=self.robot.arm.get_tip(),
+            detector=success_sensor
+        )
+        self.register_success_conditions([success_condition])
+
+    def init_episode(self, index: int) -> List[str]:
+        return ['reach the red target', 'reach the red sphere']
+
+    def variation_count(self) -> int:
+        return 1
+    
+    def base_rotation_bounds(self) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
+        return (0.0, 0.0, 0.0), (0.0, 0.0, 0.0)
+        
+    def is_static_workspace(self) -> bool:
+        return True
